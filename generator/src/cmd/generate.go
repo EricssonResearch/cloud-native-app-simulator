1992181b1fccb6c7d44c9131e1f51b7545d3511b
Aleksandra <aleksandra.obeso.duque@ericsson.com>
2022-03-25 08:11:17 +0100

    adding user prompt

--- generator/src/cmd/generate.go
+++ generator/src/cmd/generate.go
@@ -19,7 +19,50 @@ import (
 	"application-generator/src/pkg/generate"
 	"github.com/spf13/cobra"
 	"application-generator/src/pkg/model"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
+
+func yesNoPrompt(label string, def bool) bool {
+  choices := "Y/n"
+  if !def {
+      choices = "y/N"
+  }
+
+  r := bufio.NewReader(os.Stdin)
+  var s string
+
+  for {
+      fmt.Fprintf(os.Stderr, "%s (%s) ", label, choices)
+      s, _ = r.ReadString('\n')
+      s = strings.TrimSpace(s)
+      if s == "" {
+          return def
+      }
+      s = strings.ToLower(s)
+      if s == "y" || s == "yes" {
+          return true
+      }
+      if s == "n" || s == "no" {
+          return false
+      }
+  }
+}
+
+func stringPrompt(label string) string {
+  var s string
+  r := bufio.NewReader(os.Stdin)
+  for {
+      fmt.Fprint(os.Stderr, label+" ")
+      s, _ = r.ReadString('\n')
+      if s != "" {
+          break
+      }
+  }
+  return strings.TrimSpace(s)
+}
 
 var generateCmd = &cobra.Command{
 	Use:   "generate [mode] [input-file]",
@@ -32,6 +75,21 @@ var generateCmd = &cobra.Command{
 		var inputFile string
 		if mode == "random" {
 			// TODO: Change this hard-coded cluster configuration for actual user inputs
+
+			simpleMode := yesNoPrompt("Do you want to set simple configuration? (otherwise, extended)", true)
+
+			clusterNamePrefix := stringPrompt("What is your cluster name prefix?")
+			clusterNumber := stringPrompt("How many clusters do you have?")
+			nsNamePrefix := stringPrompt("What is your namespace prefix?")
+			nsNumber := stringPrompt("How many namespaces do you have?")
+
+			if !simpleMode {
+				svcMaxNumber := stringPrompt("Up to how many services do you want to have?")
+				svcReplicaMaxNumber := stringPrompt("Up to how many service replicas do you want to have?")
+				svcInMaxNumber := stringPrompt("Up to how many service endpoints do you want to have? (fan-in)")
+				svcOutMaxNumber := stringPrompt("Up to how many service calls do you want to have? (fan-out)")
+			}
+
 			clusterConfig := model.ClusterConfig{
 				Clusters: 	[]string{"cluster-1", "cluster-2", "cluster-3", "cluster-4", "cluster-5"},
 				Namespaces: []string{"ns-1", "ns-2", "ns-3"},
