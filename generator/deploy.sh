de463a8dc3b35616e9c9ae761bca7ede3a5a75e2
Mohammad Reza Saleh <mr.salehsedghpour@yahoo.com>
2022-05-04 09:07:10 +0200

    Deploy elasticsearch based on user input

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -13,12 +13,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. \n"
+    echo "Please provide the path to the description file.\n"
+else
+  # Check if logging is required
+  LOGGING=$(jq '.settings.logging' $1)
+  if [ $LOGGING == true ]; then
+    # deploy ELK stack
+    $(kubectl apply -f ./elk/namespace.yaml)
+    echo "kubectl apply -f elk/"
+    echo "Deploying Elasticsearch stack with 3 nodes ..."
+    #TODO fix no such file output in mac!
+    echo "If you see a message like the following, IGNORE it!"
+    echo "\t deploy.sh: line 29: statefulset.apps/es-cluster: No such file or directory\n"
+    echo "It might take a while ... (up to 5 minutes, enjoy your coffee! :D) "
+    $(kubectl apply -f ./elk/)
+    while [ "$( kubectl get sts es-cluster -n logging -o=jsonpath='{.status.readyReplicas}')" != 3 ]; do
+       sleep 30
+       echo "Waiting for Elasticsearch stack to be ready..."
+    done
+    KIBANA_NODE=$(kubectl get pods -l=app='kibana' -n logging -o jsonpath='{.items[*].spec.nodeName}')
+    NODE_IP=$(kubectl get pods -l=app='kibana' -n logging -o jsonpath='{.items[*].status.hostIP}')
+    echo "Kibana is deployed in $KIBANA_NODE"
+    echo "To browse kibana, just copy the following address or replace the public ip of the node with the <node-ip>:"
+    echo "\t http://<node-ip>:30000"
+    echo "\t or"
+    echo "\t http://$NODE_IP:30000"
 
-#applying manifest files to respective clusters
-for d in ./k8s/*; do
-	echo "applying deployment manifests to ${d##./k8s/}"
-	[[ -d "$d" ]] && kubectl apply --prune -f k8s/${d##./k8s/} -l version=${d##./k8s/} --context ${d##./k8s/}
-done
+    # Deploy the microservices to clusters
+    for d in ./k8s/*; do
+    	echo "applying deployment manifests to ${d##./k8s/}"
+    	[[ -d "$d" ]] && kubectl apply --prune -f k8s/${d##./k8s/} -l version=${d##./k8s/} --context ${d##./k8s/}
+    done
+  fi
+fi
