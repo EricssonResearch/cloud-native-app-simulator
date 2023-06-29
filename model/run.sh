80e89f7d5e562f23c856659b06223d6c11205f94
Hannes Mann <hannesmann2000@gmail.com>
2023-06-29 14:44:55 +0200

    Remove Python application emulator

--- model/run.sh
+++ model/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright 2021 Ericsson AB
+# Copyright 2023 Ericsson AB
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,23 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-PROTOCOL=$(jq '.endpoints[0].protocol' config/conf.json -r)
-PROCESSES=$(jq '.processes' config/conf.json -r)
-LOGGING=$(jq '.logging' config/conf.json -r)
 
-
-if [ $PROTOCOL = "http" ]; then
-  if [ $LOGGING = true ]; then
-    $(gunicorn --chdir restful -w $PROCESSES app:app -b 0.0.0.0:5000 --capture-output --log-level debug);
-  else
-    $(gunicorn --chdir restful -w $PROCESSES app:app -b 0.0.0.0:5000);
-  fi
-elif [ $PROTOCOL = "grpc" ]; then
-  $(cat config/service.proto > service.proto)
-  $(python -m grpc_tools.protoc -I. --python_out=./common --grpc_python_out=./common service.proto);
-  $(cd grpc && python pre_app.py)
-  2to3 common/ -w -n
-  # Uninstall the extra apps
-  apt remove -y 2to3 wget
-  $(cd grpc && python app.py)
-fi
+echo "Not implemented"
