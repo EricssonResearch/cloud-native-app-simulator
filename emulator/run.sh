b42bb64fef8317182e18189eb9749968e4b47d7f
Hannes Mann <hannesmann2000@gmail.com>
2023-07-18 14:13:30 +0200

    Fix protoc not finding packages

--- emulator/run.sh
+++ emulator/run.sh
@@ -7,7 +7,7 @@ cd /usr/src/app
 echo "Generating gRPC code for $SERVICE_NAME..."
 rm -Rf emulator/src/generated/*
 ln -s $GRPCIMPL emulator/src/generated/impl.go
-protoc --go_out=emulator --go_opt=module=application-emulator --go-grpc_out=emulator --go-grpc_opt=module=application-emulator $GRPCPROTO
+protoc -I$(pwd) --go_out=emulator --go_opt=module=application-emulator --go-grpc_out=emulator --go-grpc_opt=module=application-emulator $GRPCPROTO
 
 echo "Compiling service $SERVICE_NAME..."
 time go build -mod=readonly -work -ldflags "-s -w" -o /tmp/emulator-$SERVICE_NAME emulator
